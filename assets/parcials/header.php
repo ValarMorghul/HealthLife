a0c4cf82195c721a917898ee27fc2eecb0e7e459
José Victor Alves <Victor-Siqueira@outlook.com.br>
2021-06-25 12:46:57 -0300

    Adicionando os arquivos

---
+++ assets/parcials/header.php
@@ -0,0 +1,14 @@
+<header>
+	<div class="container_header">
+		<div class="logo"></div><!-- LOGO -->
+		<nav class="menu">
+			<ul>
+				<li class="selected"><a href="/Home">Home</a></li>
+				<li><a href="Contato/contato.html">Contatos</a></li>
+				<li><a href="/Home">Sobre</a></li>
+				<li><a href="/Home">Lista</a></li>
+				<li><a href="/Home">Nome</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
---
+++ index.php
@@ -0,0 +1,97 @@
+	<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>HealthLife</title>
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+</head>
+<body>
+<?php require('./assets/parcials/header.php') ?>
+
+<section class="banner">
+	<div class="banner_options">
+		<div class="banner1"><h1>
+					Exames
+				</h1>
+				<h4>if you need a doctor urgently outside of Health Life opening hours, call emergency appointmente number for emergency service.</h4>
+				<a href="">Saiba mais</a></div>
+		<div class="banner2"><h1>Saiba sobre o doutor.</h1>
+				<h4>Here at Health Life we have individual doctor's lists, click read more below to see services and current timestable for our doctor's</h4>
+
+				<a href="">Saiba mais</a></div>
+		<div class="banner3">
+			<div class="open_hours">
+				<h1>Horarios abertos!</h1>
+				<div class="banner-dias">Monday - Friday</div>
+				<div class="banner-horas">8:00 - 17:00</div>
+				<div class="banner-dias">Saturday</div>
+				<div class="banner-horas">9:30 - 17:30</div>
+				<div class="banner-dias">Sunday</div>
+				<div class="banner-horas">9:30 - 15:00</div>
+			</div>
+		</div>
+		<div class="banner4">teste</div>
+		<div class="banner5">teste</div>
+	</div>
+
+</section>
+
+<div class="clear"></div>
+<section class="home_contact">
+	<div class="welcome_text">
+		<h1 class="welcome_text_title">Seja bem-vindo a Health Life. </h1>
+		<div class="welcome_text_desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend, massa non maximus laoreet, nibh arcu dictum est, sit amet luctus dui lacus vel magna. Nam pellentesque purus ac justo blandit, a auctor neque pretium. Sed id enim vitae nisi blandit dictum. Cras fringilla porta quam non tristique. Vivamus molestie nisl dui, sit amet viverra nulla pharetra id.</div>
+		<a href="">Saiba mais!</a>
+	</div>
+	<div class="contact_form">
+		<h1 class="contact_form_title">Entre em contato conosco</h1>
+		<br>
+		<form class="contact_form_desc">
+			<label>
+				Nome:<br>
+				<input type="text" name="name">
+				<br><br>
+			</label>
+			<label>
+				Email:<br>
+				<input type="email" name="email">
+				<br><br>
+			</label>
+			<label>
+				Envie-nos uma mensagem!:
+				<br>
+				<textarea></textarea>
+			</label>
+			<br>
+			<input type="submit" name="submit" value="Enviar">
+		</form>
+	</div>
+</section>
+<section class="our_services">
+	<h1 class="our_services_title">Veja nossos serviços!</h1>
+	<div class="our_services_desc">Aenean vel gravida enim. In sed arcu ligula. Pellentesque pretium mi id ante scelerisque porta. Suspendisse libero sapien, hendrerit vitae massa vel, molestie mollis arcu. Suspendisse odio elit, finibus quis felis sodales, mattis dapibus sem. Maecenas at porttitor neque. Cras ac mi quis purus lobortis pellentesque. Praesent venenatis sagittis ultrices. Integer at varius arcu. Suspendisse aliquet ipsum non orci accumsan, at tincidunt elit malesuada. Morbi consequat lorem id malesuada porta.</div>
+	<div class="our_services_services">
+	<div class="our_services_service1"><div class="our_services_icon1"></div> <h1>Exames de x e y resultados exatos</h1>
+	<p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+</div>
+<div class="our_services_service2"><div class="our_services_icon2"></div> <h1>Exames de x e y resultados exatos</h1>
+	<p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+</div>
+<div class="our_services_service3"><div class="our_services_icon3"></div> <h1>Exames de x e y resultados exatos</h1>
+	<p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+</div>
+</div>
+<div class="our_services_services2">
+	<div class="our_services_service4"><div class="our_services_icon4"></div> <h1>Exames de x e y resultados exatos</h1>
+	<p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+</div>
+<div class="our_services_service5"><div class="our_services_icon5"></div> <h1>Exames de x e y resultados exatos</h1>
+	<p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+</div>
+<div class="our_services_service6"><div class="our_services_icon6"></div> <h1>Exames de x e y resultados exatos</h1>
+	<p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+</div>
+</div>
+</section>
+</body>
+</html>
